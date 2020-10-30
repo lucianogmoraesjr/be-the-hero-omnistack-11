57e01487d5b7a4907c59ab3d32590078fcb5ad20
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2020-10-30 01:16:02 -0300

    Finished backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-
-  return res.send('Hello World');
-
-});
+app.use(routes);
 
 app.listen(3333);
